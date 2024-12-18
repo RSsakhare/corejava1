2e931c1e7d4f43934ea0079ba853b1da8a2f3779
Rushi <dhsv@gmail.com>
2024-12-18 14:43:06 +0530

    fix issue

--- Day22/src/com/multithreading/www/Lab5Multithreading.java
+++ Day22/src/com/multithreading/www/Lab5Multithreading.java
@@ -1,45 +1,37 @@
 package com.multithreading.www;
-//import java.util.Scanner;
-class Threaduy extends Thread{
-	
-	public void run()
-	{
-		for(int i=1;i<11;i++)
-		{
-			System.out.println("i");
-		}
-	}
-}
-	
-class Threadhs extends Thread{
-		
-		public void run()
-		{
-			for(int i=1;i<11;i++)
-			{
-				System.out.println("");
-			}
-		}
-		
-	}
-	
-public class Lab5Multithreading extends Thread {
-	
-	public static void main(String args[]) {
-		 
-		int x,y;
-		scanner ob = new Scanner(System.in);
-		x=ob.nextInt();
-		y=ob.nextInt();
-		
-		
-		ThreadOne one=new ThreadOne();
-		ThreadTwo two=new ThreadTwo();
-		
-		one.start();
-		two.start();
-		ob.close();
-	
-	}
 
+import java.util.Scanner;
+
+class Threaduy extends Thread {
+    public void run() {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class Threadhs extends Thread {
+    public void run() {
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Threadhs: " + i);
+        }
+    }
+}
+
+public class Lab5Multithreading {
+    public static void main(String args[]) {
+        int x, y;
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        x = ob.nextInt();
+        System.out.print("Enter second number: ");
+        y = ob.nextInt();
+
+        Threaduy one = new Threaduy();
+        Threadhs two = new Threadhs();
+
+        one.start();
+        two.start();
+        ob.close();
+    }
 }
